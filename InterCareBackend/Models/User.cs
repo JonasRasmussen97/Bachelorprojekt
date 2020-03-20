6cbc18148a55d4434981dc5f62f00cbc3bcfc7a8
Jonas <Saltogpeber123@hotmail.com>
2020-03-20 10:09:13 +0100

    Create folders and User.cs

---
+++ InterCareBackend/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterCareBackend.Models
+{
+    interface User
+    {
+        string Email { get;set;}
+        string Password { get; set; }
+
+        string FullName { get; set; }
+
+
+    }
+}
