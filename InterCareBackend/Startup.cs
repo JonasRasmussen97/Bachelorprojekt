76caec2b241c47797dbe1e61b6a89262bba40e12
Jonas <Saltogpeber123@hotmail.com>
2020-03-27 14:12:04 +0100

    Added new vue project + front page. Furthermore COS has been disabled to allow calls from different server.

--- InterCareBackend/Startup.cs
+++ InterCareBackend/Startup.cs
@@ -23,13 +23,20 @@ namespace InterCareBackend
             Configuration = configuration;
         }
 
+
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
 
-           
 
 
             services.AddControllers();
@@ -42,6 +49,8 @@ namespace InterCareBackend
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("MyPolicy");
 
             app.UseAuthentication();
 
