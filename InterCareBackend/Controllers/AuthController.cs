aa6fa4009e8b9a9f0f35dca78f60da97499d32bf
Jonas <Saltogpeber123@hotmail.com>
2020-04-30 10:23:02 +0200

    Removed test method in backend.

--- InterCareBackend/Controllers/AuthController.cs
+++ InterCareBackend/Controllers/AuthController.cs
@@ -19,16 +19,6 @@ namespace InterCareBackend.Controllers
             return tokenString;
         }
 
-
-        [HttpGet("/api/test")]
-        public string testJWT()
-        {
-
-            return auth.getBuilder()
-                .WithAlgorithm(new HMACSHA256Algorithm())
-                .Decode(Request.Headers["Authorization"]);
-        }
-
         // This method is used to validate that a user has access to a given site in frontend. It returns the type of the logged in user based on his/her JWT.
         [HttpGet("/api/getUserType")]
         public string getUserType()
